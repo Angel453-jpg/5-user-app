574c7200a45603ef9e54d899fcb5dbe71ccf602f
Angel <angelin09ozoz@gmail.com>
2024-11-09 19:24:54 -0600

    Eliminar usuario

--- src/app/components/user-app.component.ts
+++ src/app/components/user-app.component.ts
@@ -26,4 +26,8 @@ export class UserAppComponent implements OnInit {
     this.users = [...this.users, {...user, id: new Date().getTime()}];
   }
 
+  removeUser(id: number) {
+    this.users = this.users.filter(user => user.id != id);
+  }
+
 }
--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input} from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 import {Users} from '../../models/users';
 
 @Component({
@@ -10,5 +10,14 @@ import {Users} from '../../models/users';
 export class UserComponent {
 
   @Input() users: Users[] = [];
+
+  @Output() idUserEventEmitter = new EventEmitter();
+
+  onRemoveUser(id: number): void {
+    const confirmRemove = confirm('Estas seguro que desea eliminar?')
+    if (confirmRemove) {
+      this.idUserEventEmitter.emit(id);
+    }
+  }
 
 }
