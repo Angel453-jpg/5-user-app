9b6440d6209a9c3a2f58d581868eb8bf52e6e01e
Angel <angelin09ozoz@gmail.com>
2024-11-13 10:56:57 -0600

    Consumiendo API RESTFUL GET desde el backend

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,35 +1,21 @@
 import {Injectable} from '@angular/core';
 import {Users} from '../models/users';
-import {Observable, of} from 'rxjs';
+import {Observable} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  private users: Users[] = [{
-    id: 1,
-    name: 'Angel',
-    lastName: 'González',
-    email: 'angelin09ozoz@gmail.com',
-    username: 'gabo',
-    password: '123456'
-  },
-    {
-      id: 2,
-      name: 'Rafael',
-      lastName: 'González',
-      email: 'rafael@gmail.com',
-      username: 'rafa',
-      password: '123456'
-    }
-  ];
+  private users: Users[] = [];
 
-  constructor() {
+  constructor(private http: HttpClient) {
   }
 
   findAll(): Observable<Users[]> {
-    return of(this.users);
+    // return of(this.users);
+    return this.http.get<Users[]>('http://localhost:8080/api/users');
   }
 
 }
