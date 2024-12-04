2b55fbc73c3efc3f536951f1dedd046cec7cc749
Angel <angelin09ozoz@gmail.com>
2024-12-03 21:31:45 -0600

    Subiendo proyecto a Git Hub

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,8 +4,9 @@ import {provideRouter} from '@angular/router';
 import {routes} from './app.routes';
 import {provideHttpClient, withInterceptors} from '@angular/common/http';
 import {tokenInterceptor} from './interceptors/token.interceptor';
+import { provideStore } from '@ngrx/store';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({eventCoalescing: true}),
-    provideRouter(routes), provideHttpClient(withInterceptors([tokenInterceptor]))],
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes), provideHttpClient(withInterceptors([tokenInterceptor])), provideStore()],
 };
